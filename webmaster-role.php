823054ad81c0f2dfe46635c4f914727aa45540ee
Lautaro <ljosinsaller@gmail.com>
2025-06-24 12:20:26 -0300

    update admin_capabilities

--- webmaster-role.php
+++ webmaster-role.php
@@ -10,15 +10,25 @@
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.	
 */
 
+// Capabilities de administrador que se agregarán al nuevo rol
+function get_extra_admin_capabilities() {
+    return [
+        'edit_theme_options' => true,
+        'customize' => true,
+    ];
+}
+
 function activar_crear_rol_personalizado() {
     $crear_rol = function () {
-        if (!get_role('mi_rol_personalizado')) {
-            add_role('mi_rol_personalizado', 'Mi Rol Personalizado', [
-                'read' => true,
-                'edit_posts' => false,
-                'delete_posts' => false,
-                // Agregá más capacidades si querés
-            ]);
+        if (!get_role('webmaster')) {
+            $editor_role = get_role('editor');
+            if ($editor_role) {
+                $capabilities = array_merge(
+                    $editor_role->capabilities,
+                    get_extra_admin_capabilities()
+                );
+                add_role('webmaster', 'Webmaster', $capabilities);
+            }
         }
     };
 
@@ -39,3 +49,35 @@ function activar_crear_rol_personalizado() {
 }
 
 register_activation_hook(__FILE__, 'activar_crear_rol_personalizado');
+
+// Al desactivar el plugin
+function remove_webmaster_role() {
+    $remover_rol = function() {
+        $users = get_users(['role' => 'webmaster']);
+
+        // Cambiar los usuarios con el rol 'webmaster' a 'editor'
+        foreach ($users as $user) {
+            $user->set_role('editor');
+        }
+
+        // Eliminar el rol
+        remove_role('webmaster');
+    };
+
+    if (is_multisite()) {
+        $blog_id_actual = get_current_blog_id();
+        $sitios = get_sites();
+
+        foreach ($sitios as $sitio) {
+            switch_to_blog($sitio->blog_id);
+            $remover_rol();
+            restore_current_blog();
+        }
+
+        switch_to_blog($blog_id_actual);
+    } else {
+        $remover_rol();
+    }
+}
+
+register_deactivation_hook(__FILE__, 'remove_webmaster_role');
